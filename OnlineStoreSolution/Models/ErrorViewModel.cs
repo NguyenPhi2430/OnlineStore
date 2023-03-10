14ad317ac777211c732256f622cf289ba5a49b2c
nguyenphi2430 <nguyenphi2430@gmail.com>
2023-03-11 01:55:48 +0700

    Add project files.

---
+++ OnlineStoreSolution/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineStoreSolution.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
